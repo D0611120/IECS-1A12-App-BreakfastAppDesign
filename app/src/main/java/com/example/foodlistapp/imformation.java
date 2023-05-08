5451a7ac3e352e6c595a142e1b703e490248eb77
swimchicken <swimchickenOuO@gmail.com>
2023-05-08 10:29:41 +0800

    UI stander fix

---
+++ app/src/main/java/com/example/foodlistapp/imformation.java
@@ -0,0 +1,14 @@
+package com.example.foodlistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class imformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imformation);
+    }
+}
