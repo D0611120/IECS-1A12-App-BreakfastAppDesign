2571860fe5c7c55e9874391edbfa4ca62292f187
D0611120 <stu94300@mail.fcu.edu.tw>
2023-05-29 09:22:24 +0800

    update main_tv_news

--- app/src/main/java/com/example/foodlistapp/news.java
+++ app/src/main/java/com/example/foodlistapp/news.java
@@ -9,6 +9,6 @@ public class news extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news);
+        setContentView(R.layout.activity_activity);
     }
 }
